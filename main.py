9f9c455d42c6ff893c77c75a659ee297b9c9f629
michele antonacci <m.antonacci25@studenti.uniba.it>
2023-09-10 19:38:38 +0200

    divisione squadre

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template
+import os
+
 from dotenv import load_dotenv
+from flask import Flask, render_template
 
 import api_request
 import matches_helper
-import os
 
 app = Flask(__name__)
 load_dotenv()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# FC Braciollona
 @app.route('/run', methods=['POST'])
-def run():
+def runUno():
     email_address = os.getenv("email_address")
     mailPassword = os.getenv("mailPassword")
     matchToken = os.getenv("matchToken")
-    sergio = os.getenv("sergio")
+    sergio = "michele.antonacasdsadasdassadasdasasdasci1098@gmail.com"
     antonello = os.getenv("antonello")
     fantaGoatPassword = os.getenv("fantaGoatPassword")
 
@@ -32,6 +35,26 @@ def run():
         print("It's too soon")
         return render_template('soon.html')
 
+
+# BET BET SIviglia
+@app.route('/runDue', methods=['POST'])
+def runDue():
+    email_address = os.getenv("email_address")
+    mailPassword = os.getenv("mailPassword")
+    matchToken = os.getenv("matchToken")
+    sergio = os.getenv("sergio")
+    antonello = "michele.antonacasdsadasdassadasdasasdasci1098@gmail.com"
+    fantaGoatPassword = os.getenv("fantaGoatPassword")
+
+    print("matchToken" + matchToken)
+
+    boolean = matches_helper.getIfTimeIsRunningUp(matchToken)
+    if boolean:
+        api_request.fetch_squadre_and_send_emails(email_address, mailPassword, fantaGoatPassword, sergio, antonello)
+        return render_template('fatto.html')
+    else:
+        print("It's too soon")
+        return render_template('soon.html')
 
 
 if __name__ == "__main__":
